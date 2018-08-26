e9e7443903ed80a7252c3422cba7c9f85cde974c
aebrath <anneelisabethbrath@gmail.com>
2018-08-26 16:12:05 +0200

    Adding comments.

--- movement_detector.m
+++ movement_detector.m
@@ -1,29 +1,29 @@
 %% Fix all
-accX_new = isnan(accX);
+accX_new = isnan(accX); %Checking for NaN in x-direction
 j = 1;
-for i = 1:length(accX_new)
+for i = 1:length(accX_new) %Removing all NaN, creating new vector of only real numbers
     if (accX_new(i) == 0)
         accX_clean(j) = accX(i);
         j = j + 1;
     end
 end
 
-accY_new = isnan(accY);
+accY_new = isnan(accY); %Checking for NaN in y-direction
 j = 1;
-for i = 1:length(accY_new)
+for i = 1:length(accY_new) %Removing all NaN, creating new vector of only real numbers
     if (accY_new(i) == 0)
         accY_clean(j) = accY(i);
         j = j + 1;
     end
 end
 
-accZ_new = isnan(accZ);
+accZ_new = isnan(accZ); %Checking for NaN in z-direction
 j = 1;
-for i = 1:length(accZ_new)
+for i = 1:length(accZ_new) %Removing all NaN, creating new vector of only real numbers
     if (accZ_new(i) == 0)
         accZ_clean(j) = accZ(i);
         j = j + 1;
     end
 end
 
-acctot_clean = accX_clean + accY_clean + accZ_clean;
+acctot_clean = accX_clean + accY_clean + accZ_clean; %Adding x-, y- and z-direction
